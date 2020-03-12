9e7d4ef8bf52c9e355823943e1e02829a891b137
Octopodo <orson_wells40@hotmail.com>
2020-03-13 00:13:30 +0100

    .

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,5 @@ module.exports = {
   configureWebpack: {
     devtool: 'source-map'
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/FretboardMaster/'
-    : '/'
+  publicPath: '/FretboardMaster/'
 }
