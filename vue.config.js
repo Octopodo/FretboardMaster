e509da443fff87639612fdbd3dd2591b2e8aadf2
Octopodo <orson_wells40@hotmail.com>
2020-03-12 23:03:44 +0100

    New build, env settings

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,8 @@ module.exports = {
   },
   configureWebpack: {
     devtool: 'source-map'
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/fretboard-generator/'
+    : '/'
 }
