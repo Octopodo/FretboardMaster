09adc0c244b8cb2d48112bd10890f83e83ff4933
Miguel de Mendoza <orson_wells40@hotmail.com>
2021-07-31 14:35:05 +0200

    Build

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:octopodo/Octopodo.github.io/FretboardMaster master:gh-pages
+git push -f git@github.com:Octopodo/FretboardMaster.github.io/ master:gh-pages
 
-# cd 
+cd -
